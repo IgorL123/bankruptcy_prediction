6b8b0277e03a052b312f4b39aefcf0e341816b6b
IgorL123 <Zedenbraucht@inbox.ru>
2023-05-27 17:02:40 +0300

    train end

--- model.py
+++ model.py
@@ -5,23 +5,43 @@ from optimizers import LM
 
 
 class LitMLP(pl.LightningModule):
-    def __init__(self, input_dim, output_dim, lr, nesterov, momentum, opt):
+    def __init__(self, input_dim, output_dim, lr, nesterov=False,
+                 momentum=0, opt='SGD', hiddens=(50, 40, 30, 20), ax='softplus',
+                 reg=None, lmbda=0.001, var_noise=1, where2noise=None):
         super().__init__()
 
         self.lr = lr
         self.m = momentum
         self.n = nesterov
         self.opt = opt
+        self.reg = reg
+        self.lmbda = lmbda
+        self.variance_noise = var_noise
+        self.where2noise = where2noise
+
+        if ax == 'tanh':
+            func = nn.Tanh()
+        elif ax == 'logistic':
+            func = nn.Sigmoid()
+        elif ax == 'linear':
+            func = nn.ReLU()
+        elif ax == 'softsign':
+            func = nn.Softsign()
+        elif ax == 'softplus':
+            func = nn.Softplus()
+        else:
+            raise NotImplementedError
+
         self.layers = nn.Sequential(
-            nn.Linear(input_dim, 20),
+            nn.Linear(input_dim, hiddens[0]),
             nn.ReLU(),
-            nn.Linear(20, 10),
-            nn.Tanh(),
-            nn.Linear(10, 10),
-            nn.Tanh(),
-            nn.Linear(10, 10),
-            nn.Tanh(),
-            nn.Linear(10, output_dim)
+            nn.Linear(hiddens[0], hiddens[1]),
+            func,
+            nn.Linear(hiddens[1], hiddens[2]),
+            func,
+            nn.Linear(hiddens[2], hiddens[3]),
+            func,
+            nn.Linear(hiddens[3], output_dim)
         )
         self.loss = nn.CrossEntropyLoss()
 
@@ -45,35 +65,52 @@ class LitMLP(pl.LightningModule):
                                         lr=self.lr,
                                         momentum=self.m,
                                         nesterov=self.n)
-        if self.opt == 'Adagrad':
+        elif self.opt == 'Adagrad':
             optimizer = torch.optim.Adagrad(self.parameters(),
                                             lr=self.lr)
-        if self.opt == 'RMS':
+        elif self.opt == 'RMS':
             optimizer = torch.optim.RMSprop(self.parameters(),
                                             lr=self.lr, alpha=self.m)
-        if self.opt == 'AdaDelta':
+        elif self.opt == 'AdaDelta':
             optimizer = torch.optim.Adadelta(self.parameters(),
                                              lr=self.lr, rho=self.m)
-        if self.opt == 'Adam':
+        elif self.opt == 'Adam':
             optimizer = torch.optim.Adam(self.parameters(),
-                                         lr=self.lr, betas=(self.m[0], self.m[1]))
-        if self.opt == 'Rprop':
+                                         lr=self.lr, betas=(self.m, self.m))
+        elif self.opt == 'Rprop':
             optimizer = torch.optim.Rprop(self.parameters(),
                                           lr=self.lr)
-        if self.opt == 'BFGS':
+        elif self.opt == 'BFGS':
             optimizer = torch.optim.LBFGS(self.parameters(),
                                           lr=self.lr)
-        if self.opt == 'LM':
+        elif self.opt == 'LM':
             trainable_params = [p for p in self.parameters() if p.requires_grad]
             optimizer = LM(params=trainable_params, lr=self.lr)
+        else:
+            raise NotImplementedError
 
         return optimizer
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
+        if self.where2noise == 'input' or self.where2noise == 'all':
+            n = torch.randn(x.shape, device=self.device)
+            x = x + (self.variance_noise ** 0.5) * n
+
         y_hat = self.forward(x)
         loss = self.loss(y_hat, y)
         self.log('train_loss', loss, on_epoch=True, on_step=True)
+
+        if self.reg == 'l1':
+            l1_lambda = self.lmbda
+            l1_norm = sum(p.abs().sum() for p in self.parameters())
+            loss = loss + l1_lambda * l1_norm
+            self.log('l1_norm', torch.norm(loss), on_step=True, on_epoch=True)
+        if self.reg == 'l2':
+            l2_lambda = self.lmbda
+            l2_norm = sum(p.pow(2.0).sum() for p in self.parameters())
+            loss = loss + l2_lambda * l2_norm
+            self.log('l2_norm', torch.norm(loss), on_step=True, on_epoch=True)
 
         with torch.no_grad():
             for name, p in self.named_parameters():
@@ -85,6 +122,9 @@ class LitMLP(pl.LightningModule):
                     last = self.weights_stat[name][-1]
                     self.weights_stat[name].append(abs(last - p.mean().item()))
                     self.log('layer3_change_mean', self.weights_stat[name][-1], on_epoch=True, on_step=True)
+        if self.where2noise == 'grad' or self.where2noise == 'all':
+            n = torch.randn(loss.shape, device=self.device)
+            loss = loss + (self.variance_noise ** 0.5) * n
 
         return loss
 
